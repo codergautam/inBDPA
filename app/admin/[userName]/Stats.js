97755b8989932646377eaa0777d44670bd9789e2
King-Diorr Willsun <51097023+KDW1@users.noreply.github.com>
2023-07-06 23:10:02 -0500

    Implementing client components works!

--- app/admin/[userName]/Stats.js
+++ app/admin/[userName]/Stats.js
@@ -1,26 +1,55 @@
 "use client"
 
-export default function Stats({users, views, sessions, opportunities}) {
+import { get } from "mongoose";
+import { useEffect, useState } from "react"
+
+async function getInfoData() {
+  let data = await fetch("http://localhost:3000/api/info").then((res)=> res.json());
+  return data;
+}
+
+export default function Stats() {
+    const [users, setUsers] = useState(0);
+    const [totalViews, setTotalViews] = useState(0);
+    const [sessions, setSessions] = useState(0);
+    const [opportunities, setOpportunities] = useState(0);
+
+    
+    useEffect(async ()=>{
+        setUsers("...");
+        setTotalViews("...");
+        setSessions("...");
+        setOpportunities("...");
+        let data = await getInfoData();
+        if(data.success) {
+            console.log()
+            setUsers(data.info.users);
+            setTotalViews(data.info.views);
+            setSessions(data.info.sessions);
+            setOpportunities(data.info.opportunities);
+        }
+    }, [])
+
     return (
         <div className="flex flex-row justify-between w-1/2 mx-auto mt-6">
           <span>
             <p className="text-gray-500">
-              Total Users: <span className="dark:text-white font-bold">{info ? info.users : 0}</span>
+              Total Users: <span className="dark:text-white font-bold">{users}</span>
             </p>
           </span>
           <span>
             <p className="text-gray-500">
-              Active Sessions: <span className="dark:text-white font-bold">{info ? info.sessions : 0}</span>
+              Active Sessions: <span className="dark:text-white font-bold">{sessions}</span>
             </p>
           </span>
           <span>
             <p className="text-gray-500">
-              Total Views on Platform: <span className="dark:text-white font-bold">{info ? info.views : 0}</span>
+              Total Views on Platform: <span className="dark:text-white font-bold">{totalViews}</span>
             </p>
           </span>
           <span>
             <p className="text-gray-500">
-              Total Opportunities: <span className="dark:text-white font-bold">{info ? info.opportunities : 0}</span>
+              Total Opportunities: <span className="dark:text-white font-bold">{opportunities}</span>
             </p>
           </span>
         </div>)
--- app/admin/[userName]/page.js
+++ app/admin/[userName]/page.js
@@ -1,12 +1,11 @@
-"use client"
-
 import { getRequestCookie } from "@/utils/getRequestCookie";
 import Head from "next/head";
-import { useEffect, useState } from "react";
+import Navbar from "@/components/Navbar"
 import { cookies } from "next/headers";
 import { getUserFromProfileId } from "@/utils/api";
 import Stats from "./Stats";
-// import Navbar from "@/components/navbar";
+import UserCreation from "./UserCreation";
+
 
 // async function getInfoData() {
 //   let data = await fetch("http://localhost:3000/api/info", {
@@ -59,75 +58,12 @@ export default async function Page({params}){
           <div className="text-gray-700 text-4xl text-center mt-4">
             Hello
           </div>
-          <div className="text-white text-7xl text-center font-bold">
+          <div className="text-white text-7xl text-center font-bold hover:-translate-y-2 transition duration-300 ease-in-out">
             {params.userName}
           </div>
-          <form onSubmit={handleSubmit} className="mt-4 px-6 py-4 bg-gray-700 w-1/2 mx-auto rounded"> <h1 className="text-2xl mb-6 text-center font-bold dark:text-gray-200">Create an Account</h1>
-            {/* {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
-            <div className="mb-4">
-              <label className="block text-gray-700 dark:text-gray-200 text-sm font-bold mb-2" htmlFor="name">
-                Name
-              </label>
-              <input
-                className="appearance-none rounded-lg w-full py-2 px-3 text-gray-700 dark:text-gray-200 bg-gray-200 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="name"
-                type="text"
-                placeholder="Name"
-                value={name}
-                onChange={event => setName(event.target.value)}
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-700 dark:text-gray-200 text-sm font-bold mb-2" htmlFor="email">
-                Email
-              </label>
-              <input
-                className="appearance-none rounded-lg w-full py-2 px-3 text-gray-700 dark:text-gray-200 bg-gray-200 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="email"
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={event => setEmail(event.target.value)}
-              />
-            </div>
-            <div className="mb-6">
-              <label className="block text-gray-700 dark:text-gray-200 text-sm font-bold mb-2" htmlFor="password">
-                Password
-              </label>
-              <input
-                className="appearance-none rounded-lg w-full py-2 px-3 text-gray-700 dark:text-gray-200 bg-gray-200 dark:bg-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                id="password"
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={event => setPassword(event.target.value)}
-              />
-            </div>
-            <div className="mb-6">
-              <label className="block text-gray-700 dark:text-gray-200 text-sm font-bold mb-2">
-                <input
-                  className="mr-2 leading-tight"
-                  type="checkbox"
-                  checked={rememberMe}
-                  onChange={event => setRememberMe(event.target.checked)}
-                />
-                <span className="text-sm">
-                  Remember me
-                </span>
-              </label>
-            </div>
-            <div className="flex items-center justify-center">
-              <button
-                className="pr-6 pl-6 pt-2 pb-2 text-left border  rounded-xl hover:text-blue-600 focus:text-blue-600 border-black dark:border-white text-xl "
-                type="submit"
-              >
-                {btnText}
-              </button>
-            </div>
-            <p className="text-center text-gray-700 dark:text-gray-200 mt-5">
-              Already have an account? <Link href="/auth/login" className="text-blue-600 dark:text-blue-400">Log in here</Link>
-            </p> */}
-            </form>
+          <Stats></Stats>
+          <UserCreation></UserCreation>
+          <div className="pb-52"></div>
         </div>
         )
 }
