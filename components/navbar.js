a99749c94fbe77813e928f5e17d04614ad5c6931
bananag00sekfjjgufcbdpasmn@bdpas-MacBook-Pro Blogpress % git config --global user.name bananag00se <lucymaryevans0703@gmail.com>
2023-07-07 14:32:47 -0500

    AAA

--- components/navbar.js
+++ components/navbar.js
@@ -8,14 +8,14 @@ export default function Navbar({user}) {
 
   return (
 
-<div class="flex flex-row content-center justify-between w-full h-2 bg-white border-b-2 border-blue-600 p-8 text-center">
-<div class="flex flex-row self-center text-3xl font-bold text-blue-600 cursor-pointer select-none"><img class="w-8 h-8" src="https://bdpa.org/wp-content/uploads/2020/12/f0e60ae421144f918f032f455a2ac57a.png" alt="BDPA logo"/>
+<div className="flex flex-row content-center justify-between w-full h-2 bg-white border-b-2 border-blue-600 p-8 text-center">
+<div className="flex flex-row self-center text-3xl font-bold text-blue-600 cursor-pointer select-none"><img class="w-8 h-8" src="https://bdpa.org/wp-content/uploads/2020/12/f0e60ae421144f918f032f455a2ac57a.png" alt="BDPA logo"/>
 inBDPA</div>
 
-<div class="flex flex-row space-x-8 self-center justify-end text-center ">
-    <div class="cursor-pointer text-base text-blue-600 hover:font-bold"><a href="/auth/login"/>Home</div>
-    <div class="cursor-pointer text-base text-blue-600 hover:font-bold">Sign Up</div>
-    <div class="cursor-pointer text-base text-blue-600 hover:font-bold">Log In</div>
+<div className="flex flex-row space-x-8 self-center justify-end text-center ">
+    <div className="cursor-pointer text-base text-blue-600 hover:font-bold"><a href="/auth/login"/>Home</div>
+    <div className="cursor-pointer text-base text-blue-600 hover:font-bold">Sign Up</div>
+    <div className="cursor-pointer text-base text-blue-600 hover:font-bold">Log In</div>
 </div>
 </div>
   );
