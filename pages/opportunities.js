b47249fc15400cd0a1ef04639a48ede14c45581a
Coder Gautam <gautamgxtv@gmail.com>
2023-07-07 01:29:01 -0500

    Markdown editor cool

--- pages/opportunities.js
+++ pages/opportunities.js
@@ -11,12 +11,23 @@ import { withIronSessionSsr } from 'iron-session/next';
 import { ironOptions } from '@/utils/ironConfig';
 import UserInfo from '@/components/UserInfo';
 import { getOpportunities } from '@/utils/api';
+import Modal from 'react-modal';
+import "@uiw/react-md-editor/markdown-editor.css";
+import "@uiw/react-markdown-preview/markdown.css";
+import dynamic from "next/dynamic";
+
+const MDEditor = dynamic(
+  () => import("@uiw/react-md-editor"),
+  { ssr: false }
+);
 
 // THIS PAGE DOESNT HAVE PAGINATION YET!
 // TODO: ADD PAGINATION TO THIS PAGE
 
 export default function Page({ user, opportunities }) {
   const [selectedOpportunity, setSelectedOpportunity] = useState(null);
+  const [creatingOpportunity, setCreatingOpportunity] = useState(false);
+  const [value, setValue] = useState("");
 
   const handleOpportunityClick = (opportunity) => {
     setSelectedOpportunity(opportunity);
@@ -35,6 +46,14 @@ export default function Page({ user, opportunities }) {
       <main className="flex flex-col md:flex-row">
         <div className="w-full md:w-1/3 lg:w-1/4 p-4 bg-white dark:bg-gray-800">
           <h2 className="text-xl font-bold mb-4">Opportunities</h2>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => setCreatingOpportunity(true)}>Create Opportunity</button>
+          <Modal
+            isOpen={creatingOpportunity}
+            contentLabel="Create Opportunity"
+          >
+            <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={() => setCreatingOpportunity(false)}>Close</button>
+              <MDEditor value={value} onChange={setValue} height={"90%"}/>
+            </Modal>
           <ul className="space-y-2">
             {opportunities.map((opportunity) => (
               <li
