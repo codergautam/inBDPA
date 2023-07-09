4996e0a5215910cc6a29d3989f26d1b6e2b2c51e
unknown <ishajayakumar1@gmail.com>
2023-07-09 18:16:37 -0500

    fix merge

--- pages/forgotpassword.js
+++ pages/forgotpassword.js
@@ -1,3 +1,4 @@
+
 "use client";
 import Head from 'next/head'
 import { useState } from 'react';
@@ -16,7 +17,7 @@ export default function Signup() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-white dark:bg-gray-800 text-black dark:text-white py-2">
       <Head>
-        <title>Sign Up | inBDPA</title>
+        <title>Forgot Password | inBDPA</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -55,21 +56,8 @@ export default function Signup() {
               });
             }}
           >
-            <h1 className="text-3xl mb-6 text-center font-bold dark:text-gray-200">Create an Account</h1>
-            {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
-            <div className="mb-4">
-              <label className="block text-gray-700 dark:text-gray-200 text-sm font-bold mb-2" htmlFor="name">
-                Name
-              </label>
-              <input
-                className="appearance-none rounded-lg w-full py-2 px-3 text-gray-700 dark:text-gray-200 bg-gray-200 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="name"
-                type="text"
-                placeholder="Name"
-                value={name}
-                onChange={event => setName(event.target.value)}
-              />
-            </div>
+            <h1 className="text-3xl mb-6 text-center font-bold dark:text-gray-200">Forgot Password?</h1>
+      
             <div className="mb-4">
               <label className="block text-gray-700 dark:text-gray-200 text-sm font-bold mb-2" htmlFor="email">
                 Email
@@ -83,50 +71,24 @@ export default function Signup() {
                 onChange={event => setEmail(event.target.value)}
               />
             </div>
-            <div className="mb-6">
-              <label className="block text-gray-700 dark:text-gray-200 text-sm font-bold mb-2" htmlFor="password">
-                Password
-              </label>
-              <input
-                className="appearance-none rounded-lg w-full py-2 px-3 text-gray-700 dark:text-gray-200 bg-gray-200 dark:bg-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                id="password"
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={event => setPassword(event.target.value)}
-              />
-            </div>
-            <div className="mb-6">
-              <label className="block text-gray-700 dark:text-gray-200 text-sm font-bold mb-2">
-                <input
-                  className="mr-2 leading-tight"
-                  type="checkbox"
-                  checked={rememberMe}
-                  onChange={event => setRememberMe(event.target.checked)}
-                />
-                <span className="text-sm">
-                  Remember me
-                </span>
-              </label>
-            </div>
+            
+            
             <div className="flex items-center justify-center">
               <button
                 className="pr-6 pl-6 pt-2 pb-2 text-left border  rounded-xl bg-gray-300 hover:bg-gray-400 text-white border-black dark:border-white text-xl "
                 type="submit"
               >
-                {btnText}
+                Send
               </button>
             </div>
-            <p className="text-center text-gray-700 dark:text-gray-200 mt-5">
-              Already have an account? <Link href="/auth/login" className="text-blue-600 dark:text-blue-400">Log in here</Link>
-            </p>
+           
+             
           </form>
 
         </div>
       </main>
 
     </div>
-
   )
 }
 
