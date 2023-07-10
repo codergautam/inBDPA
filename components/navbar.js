5f41c4e7de5a737517b983b9a27e26e5686ff18d
bananag00sekfjjgufcbdpasmn@bdpas-MacBook-Pro Blogpress % git config --global user.name bananag00se <lucymaryevans0703@gmail.com>
2023-07-10 12:15:04 -0500

    Fixing the Navbar

--- components/navbar.js
+++ components/navbar.js
@@ -29,9 +29,15 @@ export default function Navbar({user}) {
             <div class="flex flex-row content-center justify-between self-center portrait:default:focus-visible w-full h-1/6 bg-white border-b-2 dark:bg-gray-800 border-black dark:border-gray-500 p-4 text-center">
 
   <div className="flex flex-row self-center text-5xl font-bold text-black dark:text-gray-200 cursor-pointer select-none">
-  <div>in</div>
+  <Link href="http://localhost:3000">
+    in
+  </Link>
+  <Link href="http://localhost:3000">
   <img className="w-10 h-10 self-center" src="https://bdpa.org/wp-content/uploads/2020/12/f0e60ae421144f918f032f455a2ac57a.png" alt="BDPA logo"/>
-  <div>dpa</div>
+  </Link>
+  <Link href="http://localhost:3000">
+    dpa
+  </Link>
             </div>
 
 <div className="flex items-center mt-4 md:mt-0 self-center">
