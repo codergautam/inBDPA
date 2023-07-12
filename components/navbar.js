5bbca724d9e6694b62070bcde955944dde572942
sckjesus <samuelckaspar@gmail.com>
2023-07-12 18:38:13 -0500

    navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,16 +1,19 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
-import { useState } from 'react';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faMask, faBars } from "@fortawesome/free-solid-svg-icons"
+import { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMask, faBars } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar({ user }) {
   const router = useRouter();
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
   const leaveImpersonation = async () => {
-    let data = await fetch("/api/admin/returnToAdmin").then(res => res.json());
+    let data = await fetch("/api/admin/returnToAdmin").then((res) =>
+      res.json()
+    );
     if (data.success) {
       router.push("/");
     } else {
@@ -19,17 +22,21 @@ export default function Navbar({ user }) {
   };
 
   return (
-    <div className={`flex-1 flex flex-col sm:flex-row sm:items-center sm:justify-between py-2 px-2`}>
+    <div
+      className={`flex-1 flex flex-col sm:flex-row sm:items-center sm:justify-between py-2 px-2`}
+    >
       <div className="flex flex-col sm:flex-row content-center justify-between self-center portrait:default:focus-visible w-full h-1/6 bg-white border-b-2 dark:bg-gray-800 border-black dark:border-gray-500 p-4 text-center">
         <div className="flex flex-row self-center text-2xl sm:text-4xl md:text-5xl font-bold text-black dark:text-gray-200 cursor-pointer select-none">
-
           <Link href="http://localhost:3000">
-            <img className="w-22 h-16 self-center" src="https://i.imgur.com/uWlkxIt.png" alt="BDPA logo"/>
+            <img
+              className="w-22 h-16 self-center"
+              src="https://i.imgur.com/uWlkxIt.png"
+              alt="BDPA logo"
+            />
           </Link>
-
         </div>
 
-        <div className="flex flex-col sm:flex-row items-center mt-4 md:mt-0 self-center">
+        <div className="flex flex-col sm:flex-row items-center ">
           {user && (
             <div className={`sm:flex `}>
               {user.impersonating && (
@@ -38,7 +45,10 @@ export default function Navbar({ user }) {
                   className="w-min min-w-max group flex text-sm font-medium text-gray-700 rounded dark:text-gray-200 hover:font-bold"
                 >
                   Leave Impersonation
-                  <FontAwesomeIcon className="my-auto ml-2 text-gray-700 group-hover:text-white transition duration-300 ease-in-out w-5 h-5" icon={faMask} />
+                  <FontAwesomeIcon
+                    className="my-auto ml-2 text-gray-700 group-hover:text-white transition duration-300 ease-in-out w-5 h-5"
+                    icon={faMask}
+                  />
                 </button>
               )}
               {user.type === "administrator" && (
@@ -72,15 +82,5 @@ export default function Navbar({ user }) {
         </div>
       </div>
     </div>
-<div className="flex flex-row content-center justify-between w-full h-2 bg-white border-b-2 border-blue-600 p-8 text-center">
-<div className="flex flex-row self-center text-3xl font-bold text-blue-600 cursor-pointer select-none"><img class="w-8 h-8" src="https://bdpa.org/wp-content/uploads/2020/12/f0e60ae421144f918f032f455a2ac57a.png" alt="BDPA logo"/>
-inBDPA</div>
-
-<div className="flex flex-row space-x-8 self-center justify-end text-center ">
-    <div className="cursor-pointer text-base text-blue-600 hover:font-bold"><a href="/auth/login"/>Home</div>
-    <div className="cursor-pointer text-base text-blue-600 hover:font-bold">Sign Up</div>
-    <div className="cursor-pointer text-base text-blue-600 hover:font-bold">Log In</div>
-</div>
-</div>
   );
 }
