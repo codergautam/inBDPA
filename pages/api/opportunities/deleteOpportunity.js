dd1181ade62ae200e4b286279b94d0326531a611
King-Diorr Willsun <51097023+KDW1@users.noreply.github.com>
2023-07-12 21:27:46 -0500

    Finished all CRUD Operations for Opportunities

---
+++ pages/api/opportunities/deleteOpportunity.js
@@ -0,0 +1,17 @@
+import { deleteOpportunity } from "@/utils/api";
+import { withIronSessionApiRoute } from "iron-session/next";
+
+import { ironOptions } from "@/utils/ironConfig";
+
+export default withIronSessionApiRoute(handler, ironOptions);
+
+ async function handler(req, res) {
+  const { opportunity_id } = req.body
+  const data = (await deleteOpportunity(opportunity_id));
+  console.log("Data: ", data)
+  if(!data.success) {
+    console.log("Error")
+    return res.json({success: false, error: "Couldn't delete opportunity!"});
+  }
+  res.json({success: true});
+}
---
+++ pages/api/opportunities/editOpportunity.js
@@ -0,0 +1,20 @@
+import { updateOpportunity } from "@/utils/api";
+import { withIronSessionApiRoute } from "iron-session/next";
+
+import { ironOptions } from "@/utils/ironConfig";
+import { convertHexToBuffer } from "@/utils/encryptPassword";
+export default withIronSessionApiRoute(handler, ironOptions);
+
+ async function handler(req, res) {
+  const { opportunity_id, title, contents } = req.body
+  const user = req.session.user;
+  if(!user) {
+    return res.json({success: false, error: "Not logged in"});
+  }
+  const creator_id = req.session.user.id;
+  console.log("Body for new Opportunity:", { title, contents, creator_id })
+  let data = await updateOpportunity(opportunity_id, {title, contents});
+  console.log("Data: ")
+  console.log(data)
+  res.json(data);
+}
