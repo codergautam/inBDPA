f0fc1feb7a032aed6c32ac1693167f0d3bbe388c
sckjesus <samuelckaspar@gmail.com>
2023-07-13 17:46:26 -0500

    navbar

--- components/navbar.js
+++ components/navbar.js
@@ -22,22 +22,25 @@ export default function Navbar({ user }) {
   };
 
   return (
-    <div className="flex flex-col sm:flex-row content-center justify-between self-center portrait:default:focus-visible w-full h-1/6 bg-white border-b-2 dark:bg-gray-800 border-black dark:border-gray-500 p-4 text-center">
-      <Link href="/">
-        <img
-          className="w-16 self-center dark:opacity-0"
-          src="https://i.imgur.com/uWlkxIt.png"
-          alt="BDPA logo"
-        />
-      </Link>
-      <Link href="/">
-        <img
-          className="h-16 self-center opacity-0 dark:opacity-100 pl-40"
-          src="https://i.imgur.com/GOmYSIZ.png"
-          alt="BDPA logo"
-        />
-      </Link>
-      <div className="flex flex-col sm:flex-row items-center ">
+    <div className="flex flex-col sm:flex-row items-center justify-between self-center portrait:default:focus-visible w-full h-1/6 bg-white border-b-2 dark:bg-gray-800 border-black dark:border-gray-500 p-4 text-center">
+      <div className="h-min">
+        <Link href="/">
+          <img
+            className="h-36 self-center dark:hidden inline"
+            src="https://i.imgur.com/uWlkxIt.png"
+            alt="BDPA logo"
+          />
+        </Link>
+        <Link href="/">
+          <img
+            className="w-36 self-center hidden dark:inline"
+            src="https://i.imgur.com/GOmYSIZ.png"
+            alt="BDPA logo"
+          />
+        </Link>
+      </div>
+
+      <div className="flex flex-row items-center ">
         {user && user.impersonating && (
           <>
             <button
@@ -46,7 +49,7 @@ export default function Navbar({ user }) {
             >
               Leave Impersonation
               <FontAwesomeIcon
-                className="my-auto ml-2 text-gray-700 group-hover:text-white transition duration-300 dark:text-gray-200 ease-in-out w-5 h-5"
+                className="my-auto ml-2 text-gray-700 group-hover:text-white transition duration-300 ease-in-out w-5 h-5"
                 icon={faMask}
               />
             </button>
@@ -56,7 +59,7 @@ export default function Navbar({ user }) {
           <>
             <Link
               href={`/admin/${user.link}`}
-              className="px-2 py-1 text-xs sm:text-sm md:text-lg font-medium dark:text-gray-200"
+              className="px-2 py-1 text-xs sm:text-sm md:text-lg font-medium"
             >
               Admin
             </Link>
@@ -66,19 +69,19 @@ export default function Navbar({ user }) {
           <>
             <Link
               href="/"
-              className="px-2 py-1 text-xs sm:text-sm md:text-lg font-medium dark:text-gray-200"
+              className="px-2 py-1 text-xs sm:text-sm md:text-lg font-medium"
             >
               Home
             </Link>
             <Link
               href="../opportunities"
-              className="px-2 py-1 text-xs sm:text-sm md:text-lg font-medium dark:text-gray-200"
+              className="px-2 py-1 text-xs sm:text-sm md:text-lg font-medium"
             >
               Opportunities
             </Link>
             <Link
               href="../api/auth/logout"
-              className="px-2 py-1 text-xs sm:text-sm md:text-lg font-medium dark:text-gray-200"
+              className="px-2 py-1 text-xs sm:text-sm md:text-lg font-medium"
             >
               Logout
             </Link>
@@ -87,14 +90,14 @@ export default function Navbar({ user }) {
           <>
             <Link
               href="../auth/login"
-              className="px-2 py-1 text-xs sm:text-sm md:text-lg font-medium dark:text-gray-200"
+              className="px-2 py-1 text-xs sm:text-sm md:text-lg font-medium"
             >
               Login
             </Link>
 
             <Link
               href="../auth/signup"
-              className="px-2 py-1 text-xs sm:text-sm md:text-lg font-medium dark:text-gray-200"
+              className="px-2 py-1 text-xs sm:text-sm md:text-lg font-medium"
             >
               Sign up
             </Link>
