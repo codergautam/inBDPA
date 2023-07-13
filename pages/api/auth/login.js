b2993bd482b62f27de895f2cc86a24d0389309fb
Coder Gautam <gautamgxtv@gmail.com>
2023-07-13 12:05:23 -0500

    Remember me functionality

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -2,32 +2,30 @@ import { authenticateUser, getUserByUsername, loginUser } from "@/utils/api";
 import { NextResponse } from "next/server";
 import { withIronSessionApiRoute } from "iron-session/next";
 
-import { ironOptions } from "@/utils/ironConfig";
+import { getIronOptions } from "@/utils/ironConfig";
 import { convertHexToBuffer } from "@/utils/encryptPassword";
-export default withIronSessionApiRoute(handler, ironOptions);
 
- async function handler(req, res) {
-  if(req.method !== "POST") return res.send({ error: "Method not allowed" });
+async function handler(req, res) {
+  if (req.method !== "POST") return res.send({ error: "Method not allowed" });
 
-
-  const { username, password } = req.body;
+  const { username, password, rememberMe } = req.body;
   if (!password || !username) {
     return res.send({ error: "Missing required fields" });
   }
 
   let user = await loginUser(username, password);
-  console.log("User:")
-  console.log(user)
+
   // store in session
-  if(user.success) {
-    console.log("Logging in")
+  if (user.success) {
     req.session.user = {id: user.user.user_id, username: user.user.username, email: user.user.email, type: user.user.type, link: user.user.link, salt: user.user.salt, key: user.user.key};
-    console.log(req.session.user);
     await req.session.save();
   } else {
     return res.send({error: user.error});
   }
 
-
   return res.send(user);
+}
+
+export default function (req, res) {
+  return withIronSessionApiRoute(handler, getIronOptions(req.body.rememberMe ?? false))(req, res);
 }
