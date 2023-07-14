6ec5d10f47d78fdee41aedc6d031e49ce34da5a7
sckjesus <samuelckaspar@gmail.com>
2023-07-14 13:21:12 -0500

    homepage

--- pages/index.js
+++ pages/index.js
@@ -46,18 +46,18 @@ export default function Home(props) {
       <main className="flex flex-col items-center justify-center w-7/8 px-4 md:px-0 flex-1 md:w-5/6 3xl:w-2/3 mx-auto text-center">
         {/* Welcome section */}
         <div className="flex flex-col items-center justify-center h-screen">
-          <h1 className="text-4xl sm:text-6xl font-bold flex items-center">
+          <h1 className="text-4xl sm:text-6xl font-bold items-center">
             <span>Welcome to </span>
               <Link href="/">
                 <img
-                  className="w-28 ml-2 sm:w-48 self-center dark:hidden inline"
+                  className="w-28 sm:w-48 self-center dark:hidden inline"
                   src="https://cdn.discordapp.com/attachments/1121115967120998540/1129195814447759410/Screenshot_2023-07-13_at_6.34.43_PM-PhotoRoom.png-PhotoRoom.png"
                   alt="BDPA logo"
                 />
               </Link>
               <Link href="/">
                 <img
-                  className="w-28 ml-2 sm:w-48 self-center hidden dark:inline"
+                  className="w-28 sm:w-48 self-center hidden dark:inline"
                   src="https://cdn.discordapp.com/attachments/1121115967120998540/1129463854536085557/168935544518199980.png"
                   alt="BDPA logo"
                 />
