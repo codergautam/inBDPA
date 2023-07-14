5c036d014e5e83d185a8426edf241862ed0a1cd0
sckjesus <samuelckaspar@gmail.com>
2023-07-14 12:28:44 -0500

    homepage

--- pages/index.js
+++ pages/index.js
@@ -50,15 +50,15 @@ export default function Home(props) {
             <span>Welcome to </span>
               <Link href="/">
                 <img
-                  className="w-28 sm:w-36 self-center dark:hidden inline"
+                  className="w-28 sm:w-42 self-center dark:hidden inline"
                   src="https://cdn.discordapp.com/attachments/1121115967120998540/1129195814447759410/Screenshot_2023-07-13_at_6.34.43_PM-PhotoRoom.png-PhotoRoom.png"
                   alt="BDPA logo"
                 />
               </Link>
               <Link href="/">
                 <img
-                  className="w-28 sm:w-36 self-center hidden dark:inline"
-                  src="https://cdn.discordapp.com/attachments/1121115967120998540/1129197625619513414/Screenshot_2023-07-13_at_6.28.43_PM-PhotoRoom.png-PhotoRoom.png"
+                  className="w-28 ml-2 sm:w-48 self-center hidden dark:inline"
+                  src="https://cdn.discordapp.com/attachments/1121115967120998540/1129463854536085557/168935544518199980.png"
                   alt="BDPA logo"
                 />
               </Link>
