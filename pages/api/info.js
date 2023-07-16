bcff7fc11410c2965bc88c4013d556ceb0a65448
King-Diorr Willsun <51097023+KDW1@users.noreply.github.com>
2023-07-15 19:10:28 -0500

    Protected info route

--- pages/api/info.js
+++ pages/api/info.js
@@ -1,6 +1,10 @@
 import { getInfo } from "@/utils/api";
 
 export default async function handler(req, res) {
+    if(req.session.user.type != "administrator") {
+        res.json({success: false, error: "Unauthorized"})
+    }
+
     let data = await getInfo();
     res.json(data);
 }
