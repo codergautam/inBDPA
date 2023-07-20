9aabaa5a1bf7f1e0119dbae94db018e92c3599a0
Coder Gautam <gautamgxtv@gmail.com>
2023-07-20 13:04:15 -0500

    Opportunity editor new 1/2

--- pages/opportunities.js
+++ pages/opportunities.js
@@ -15,7 +15,7 @@ import { faPenNib, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 const MDEditor = dynamic(
   () => import("@uiw/react-md-editor"),
-  { ssr: false }
+  { ssr: false, loading: () => <div className="flex items-center justify-center py-12"><div className="animate-spin rounded-full h-10 w-10 border-b-2 border-blue-500"></div></div> }
 );
 
 export default function Page({ user }) {
@@ -28,6 +28,7 @@ export default function Page({ user }) {
   const [opps, setOpps] = useState([]);
   const [loading, setLoading] = useState(false);
   const lastOppRef = useRef(null);
+  const [mdEditorMode, setMdEditorMode] = useState('live');
 
 
 
@@ -35,6 +36,7 @@ const loadOpportunities = async (first=false) => {
 
   if(!lastOppRef.current &&!first) return;
   setLoading(true);
+
 
   console.trace("loading opportunities", lastOppRef.current)
   let data = await fetch("/api/opportunities/getOpportunities", {
@@ -56,7 +58,6 @@ const loadOpportunities = async (first=false) => {
   }
   setLoading(false);
 };
-
 const debounce = (func, wait) => {
   let timeout;
   return (...args) => {
@@ -74,14 +75,25 @@ const handleScroll = debounce(() => {
   }
 }, 100);
 
+function handleResize() {
+  if (window.innerWidth > 768) {
+    setMdEditorMode('live');
+  } else {
+    setMdEditorMode('edit');
+  }
+}
+
 useEffect(() => {
   loadOpportunities(true);
   window.addEventListener('scroll', () => {
     setLoading(true)
     handleScroll();
   });
+  window.addEventListener('resize', handleResize);
   return () => {
     window.removeEventListener('scroll', handleScroll);
+    // And this line
+    window.removeEventListener('resize', handleResize);
   };
 }, []);
 
@@ -162,6 +174,7 @@ useEffect(() => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+
       <div>
         <Navbar user={user} />
       </div>
@@ -180,21 +193,100 @@ useEffect(() => {
           </div>
         ) : null}
 
-        <Modal
-          isOpen={creatingOpportunity}
-          contentLabel="Create Opportunity"
+<Modal
+  isOpen={creatingOpportunity}
+  contentLabel="Create Opportunity"
+>
+  <div
+    className="
+      flex flex-col items-center justify-center
+      bg-white dark:bg-gray-800 w-full h-full p-6 pt-10
+      relative
+    "
+    style={{
+      marginBottom: "-20px",
+      marginTop:"-20px",
+      marginLeft:"-24px",
+      marginRight:"-24px",
+      width:"calc(100% + 48px)",
+      height:"calc(100% + 48px)"
+    }}
+  >
+    <button
+      onClick={() => setCreatingOpportunity(false)}
+      className="
+        absolute top-2 right-2
+        bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded
+      "
+    >
+      Close
+    </button>
 
-        >
-          <div className='bg-white dark:bg-gray-800 w-full h-full' style={{marginBottom: "-20px", marginTop:"-20px", marginLeft:"-24px", marginRight:"-24px", width:"calc(100% + 48px)", height:"calc(100% + 48px)"}}>
-          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={() => setCreatingOpportunity(false)}>Close</button>
-          <div className='flex flex-col mt-8'>
-            <label htmlFor="" className="text-3xl font-bold text-black">Title:</label>
-            <input onChange={e => setTitle(e.target.value)} value={title} type="text" className='mb-4 outline-none text-black border-b-2 w-1/2' />
-          </div>
-          <MDEditor className='mt-4' value={value} onChange={setValue} height={"90%"} />
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2" onClick={makeNewOpportunity}>Create Opportunity</button>
-          </div>
-        </Modal>
+    <div className='mt-8 w-full max-w-xl'>
+      <label
+        htmlFor=""
+        className="block text-3xl font-bold text-black dark:text-white mb-2"
+      >
+        Title:
+      </label>
+
+      <input
+        onChange={e => setTitle(e.target.value)}
+        value={title}
+        type="text"
+        className='
+          mb-4 outline-none text-black border-b-2 w-full
+          px-3 py-2
+          focus:border-blue-500
+        '
+      />
+    </div>
+
+   <div className="md:hidden flex items-center bg-white dark:bg-gray-800 py-2 px-4 rounded mb-4">
+  <label className="flex items-center mr-4">
+    <input
+      type="radio"
+      value="edit"
+      checked={mdEditorMode === 'edit'}
+      onChange={() => setMdEditorMode('edit')}
+      className="form-radio text-blue-500 dark:text-blue-300 h-4 w-4 text-xs mr-2"
+    />
+    <span className="text-black dark:text-white">Edit</span>
+  </label>
+  <label className="flex items-center">
+    <input
+      type="radio"
+      value="preview"
+      checked={mdEditorMode === 'preview'}
+      onChange={() => setMdEditorMode('preview')}
+      className="form-radio text-blue-500 dark:text-blue-300 h-4 w-4 text-xs mr-2"
+    />
+    <span className="text-black dark:text-white">Preview</span>
+  </label>
+</div>
+
+
+    <MDEditor
+      className='mt-4 w-full'
+      value={value}
+      onChange={setValue}
+      height={"90%"}
+      preview={mdEditorMode}
+    />
+
+    <button
+      onClick={makeNewOpportunity}
+      className="
+        bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2
+        self-end
+      "
+    >
+      Create Opportunity
+    </button>
+  </div>
+</Modal>
+
+
 
         <Modal
           isOpen={editingOpportunity}
