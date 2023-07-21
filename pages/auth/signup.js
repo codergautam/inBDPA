73fc0ebe60d155e426257dde6f9a331235a5d7c3
sckjesus <samuelckaspar@gmail.com>
2023-07-21 15:14:02 -0500

    really good navbar changes

--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -142,7 +142,7 @@ export default function Signup() {
                     // }}
                     onChange={(event) => setName(event.target.value)}
                     required=""
-                    placeholder="John"
+                    placeholder="john"
                     name="name"
                     type="text"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
