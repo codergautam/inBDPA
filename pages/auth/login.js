2b55f1b92c65cc275cce05c7b0ffe3fe98a3903a
sckjesus <samuelckaspar@gmail.com>
2023-07-29 13:09:55 -0500

    signup/login height

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -144,7 +144,7 @@ export default function Login() {
   };
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900 h-screen">
+    <section className="bg-gray-50 dark:bg-gray-900 h-full">
       <div className="h-0 w-screen">
         <Navbar />
       </div>
@@ -192,7 +192,7 @@ export default function Login() {
         <title>Login | inBDPA</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-full lg:py-0 min-h-screen">
         {/* <Link
           href="/"
           className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
