06df8d26757c1465b16b2ea2c33696824885add0
King-Diorr Willsun <51097023+KDW1@users.noreply.github.com>
2023-07-29 12:02:11 -0500

    Shoudl be fixed

--- pages/api/opportunities/editOpportunity.js
+++ pages/api/opportunities/editOpportunity.js
@@ -1,4 +1,4 @@
-import { updateOpportunity, updateOpportunityMongo } from "@/utils/api";
+import { getOpportunity, getOpportunityMongo, updateOpportunity, updateOpportunityMongo } from "@/utils/api";
 import { withIronSessionApiRoute } from "iron-session/next";
 
 import { ironOptions } from "@/utils/ironConfig";
@@ -6,13 +6,37 @@ import { convertHexToBuffer } from "@/utils/encryptPassword";
 export default withIronSessionApiRoute(handler, ironOptions);
 
  async function handler(req, res) {
+  
+  if(req.session.user.type == "inner") {
+    return res.json({success: false, message: "smh, inners :(", error: "Unauthorized"})
+  }
+  
+
   const { opportunity_id, title, contents } = req.body
   const user = req.session.user;
   if(!user) {
     return res.json({success: false, error: "Not logged in"});
   }
+
   const creator_id = req.session.user.id;
   console.log("Body for new Opportunity:", { title, contents, creator_id })
+
+  //Checking if you own the opportunity
+  let opp =  await getOpportunityMongo(opportunity_id)
+  if(!opp) {
+    opp = await getOpportunity(opportunity_id)
+  }
+
+  
+  //Opportunity doesn't exist
+  if(!opp) {
+    return res.json({success: false, error: "No such opportunity"})
+  }
+  
+  if(creator_id != opp.creator_id) {
+    return res.json({success: false, error: "Unauthorized"})
+  }
+
   let data = await updateOpportunity(opportunity_id, {title, contents});
   if(data.success) {
   await updateOpportunityMongo(opportunity_id, {title, content: contents}, true);
