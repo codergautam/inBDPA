22e4c212ead9efd9c2dcf08c152052cef2b1b0cc
Coder Gautam <gautamgxtv@gmail.com>
2023-07-29 21:09:49 -0500

    Search fixes

--- pages/_app.js
+++ pages/_app.js
@@ -204,6 +204,7 @@ const handleRouteChangeStart = async (url, first=false) => {
     //     <Component {...pageProps} />
     //   )}
     // </>
+    
     if(loading) {
     return (
       <div className="fixed inset-0 flex flex-col items-center justify-center bg-white dark:bg-gray-800">
