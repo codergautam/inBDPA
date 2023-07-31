37c024ee5bf8426dde998980596dbab3a76c8c35
bananag00sekfjjgufcbdpasmn@bdpas-MacBook-Pro Blogpress % git config --global user.name bananag00se <lucymaryevans0703@gmail.com>
2023-07-31 12:53:04 -0500

    Navbar Search

--- components/navbar.js
+++ components/navbar.js
@@ -69,7 +69,7 @@ export default function Navbar({ user, queryText = "" }) {
             <path d="M 3 8 A 2.0002 2.0002 0 1 0 3 12 L 47 12 A 2.0002 2.0002 0 1 0 47 8 L 3 8 z M 3 23 A 2.0002 2.0002 0 1 0 3 27 L 47 27 A 2.0002 2.0002 0 1 0 47 23 L 3 23 z M 3 38 A 2.0002 2.0002 0 1 0 3 42 L 47 42 A 2.0002 2.0002 0 1 0 47 38 L 3 38 z"></path>
           </svg>{" "}
           
-        </div><div className="relative w-full md:w-72 lg:w-96 h-10 hidden md:inline">
+        </div><div className="relative w-full md:w-72 lg:w-96 h-10 ml-96 hidden md:inline">
             {user && (
               <>
                 <input
