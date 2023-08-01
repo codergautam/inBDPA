e63ccb79ec0a3e1104fdcb1f8bdf86898c94e24a
sckjesus <samuelckaspar@gmail.com>
2023-08-01 10:36:55 -0500

    responsive

--- components/ConnectionList.js
+++ components/ConnectionList.js
@@ -102,7 +102,7 @@ const ConnectionList = ({
       <Modal
         isOpen={isModalOpen}
         overlayClassName="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 "
-        className="border border-gray-200 p-4 bg-white dark:bg-gray-900 max-w-2xl mx-auto mt-12 h-4/5 w-3/4 lg:w-1/4 overflow-y-scroll pb-3"
+        className="border border-gray-200 p-4 bg-white dark:bg-gray-900 max-w-2xl mx-auto mt-12 h-4/5 w-full sm:w-3/4 md:w-1/2 lg:2/5 xl:w-1/4 overflow-y-scroll pb-3"
         onRequestClose={closeModal}
         ariaHideApp={false} // Added to prevent the warning about appElement not being defined
       >
@@ -111,7 +111,7 @@ const ConnectionList = ({
           onClick={closeModal}
         >
           <svg
-            className="fill-current text-red-500 hover:text-red-400 w-7 h-7 p-1 stroke-2 ml-auto rounded-full bg-black"
+            className="fill-current text-red-500 hover:text-red-400 darkhover:text-red-400 w-7 h-7 p-1 stroke-2 ml-auto rounded-full bg-black"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
@@ -198,10 +198,10 @@ const ConnectionList = ({
                     }
                     className={
                       user.yourDepth == 1
-                        ? `h-fit overflow-visible relative flex items-center gap-6 text-white text-lg font-semibold cursor-pointer rounded-lg bg-gray-100 dark:bg-blue-600 hover:bg-blue-700 py-2 mx-auto dark:shadow-2xl duration-200 ease-in-out transition w-full my-2`
+                        ? `h-fit overflow-visible relative flex items-center gap-6 text-white text-lg font-semibold cursor-pointer rounded-lg bg-gray-100 dark:bg-blue-600 hover:bg-blue-700 dark:hover:bg-blue-700 py-2 mx-auto dark:shadow-2xl duration-200 ease-in-out transition w-full my-2`
                         : user.yourDepth == 2
-                        ? `h-fit overflow-visible relative flex items-center gap-6 text-white font-semibold italic flex-row cursor-pointer rounded-lg bg-gray-100 dark:bg-gray-700 hover:bg-gray-600 my-2 text-lg py-2 mx-auto dark:shadow-2xl duration-200 ease-in-out transition w-full`
-                        : `h-fit overflow-visible relative flex items-center gap-6 text-gray-300 font-normal hover:text-white flex-row cursor-pointer rounded-lg bg-gray-100 dark:bg-gray-700 hover:bg-gray-600 text-sm my-2 py-2 mx-auto dark:shadow-md duration-100 ease-in-out transition w-full`
+                        ? `h-fit overflow-visible relative flex items-center gap-6 text-white font-semibold italic flex-row cursor-pointer rounded-lg bg-gray-100 dark:bg-gray-700 hover:bg-gray-600 dark:hover:bg-gray-600 my-2 text-lg py-2 mx-auto dark:shadow-2xl duration-200 ease-in-out transition w-full`
+                        : `h-fit overflow-visible relative flex items-center gap-6 text-gray-300 font-normal hover:text-white dark:hover:text-white flex-row cursor-pointer rounded-lg bg-gray-100 dark:bg-gray-700 hover:bg-gray-600 dark:hover:bg-gray-600 text-sm my-2 py-2 mx-auto dark:shadow-md duration-100 ease-in-out transition w-full`
                     }
                   >
                     {i % 2 === 0 && (
