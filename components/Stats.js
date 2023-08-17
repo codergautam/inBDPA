10eed95ff504bee89f5c371289ac51711b43f2e6
King-Diorr Willsun <51097023+KDW1@users.noreply.github.com>
2023-08-17 16:36:24 -0400

    Adding correct statuses

--- components/Stats.js
+++ components/Stats.js
@@ -1,6 +1,11 @@
 // components/Stats.js
 // This file contains the code for the Stats component. The Stats component is responsible for fetching data from the "/api/info" endpoint and displaying various statistics about the platform. It makes use of the useState, useEffect, useRef, and fetch functions from React. The component also includes a StatItem component that is used to display individual statistics. The component fetches data from the "/api/info" endpoint and updates the state variables with the fetched data. It also sets up an interval to fetch data every 30 seconds. The fetched data is then displayed using the StatItem component.
+import { faArrowDown, faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/router";
+import { list } from "postcss";
 import { useEffect, useRef, useState } from "react";
+import Link from "next/link";
 
 async function getInfoData() {
   let data = await fetch("/api/info").then((res) => res.json());
@@ -14,9 +19,11 @@ function numberWithCommas(x) {
 export default function Stats() {
   const [users, setUsers] = useState("...");
   const [totalViews, setTotalViews] = useState("...");
-  const [sessions, setSessions] = useState("...");
+  const [sessionCount, setSessionCount] = useState("...");
+  const [sessions, setSessions] = useState([])
   const [opportunities, setOpportunities] = useState("...");
   const [articles, setArticles] = useState("...");
+  const [listOfUsers, setListOfUsers] = useState([])
 
   const intervalRef = useRef();
 
@@ -27,11 +34,14 @@ export default function Stats() {
     // setOpportunities("...");
     let data = await getInfoData();
     if (data.success) {
-      setUsers(data.info.users);
-      setTotalViews(data.info.views);
-      setSessions(data.info.sessions);
-      setOpportunities(data.info.opportunities);
-      setArticles(data.info.articles);
+      let info = data.info
+      setUsers(info.users);
+      setTotalViews(info.views);
+      setSessionCount(info.sessionCount);
+      setSessions(info.sessions)
+      setOpportunities(info.opportunities);
+      setArticles(info.articles);
+      setListOfUsers(info.userList)
     }
   }
 
@@ -44,25 +54,95 @@ export default function Stats() {
   }, []);
 
   return (
-    <div className="flex flex-col lg:flex-row justify-center mt-6 mx-auto w-full sm:w-4/5 lg:w-3/4 ">
+    <div className="flex flex-col">
+    <div className="flex lg:flex-row justify-center mt-6 mx-auto w-full sm:w-4/5 lg:w-full ">
       <StatItem label="Total Users" value={users} />
-      <StatItem label="Active Sessions" value={sessions} />
       <StatItem label="Total Views on Platform" value={totalViews} />
       <StatItem label="Total Opportunities" value={opportunities} />
       <StatItem label="Total Articles" value={articles} />
     </div>
+          <StatItem sessions={JSON.parse(JSON.stringify(sessions))} listOfUsers={JSON.parse(JSON.stringify(listOfUsers))} type={"sessions"} label="Active Sessions" value={sessionCount} />
+
+    </div>
   );
 }
 
-function StatItem({ label, value }) {
+function StatItem({ label, listOfUsers, sessions, type, value }) {
+  const [open, setOpen] = useState(false)
+  const [userList, setUserList] = useState(listOfUsers)
+  const [sessionList, setSessionList] = useState(sessions)
+  const router = useRouter()
+  useEffect(() => {
+    setUserList(listOfUsers)
+    setSessionList(sessions)
+    console.log(listOfUsers)
+    console.log(sessions)
+    console.log("hi")
+    console.log("User List: ", userList)
+  }, [listOfUsers, sessions])
+
+  const properLink = session => {
+    console.log(session)
+  }
+
   return (
-    <div className="lg:w-1/4 text-center m-4 grow flex flex-col items-center justify-center dark:bg-gray-700 rounded-lg shadow-none bg-white border-2 dark:border-none dark:shadow-md p-4 transition-all duration-300 transform hover:scale-105">
-      <p className="text-center font-semibold text-black dark:font-semibold dark:text-white text-md lg:text-lg xl:text-xl mb-2">
-        {label}:
-      </p>
-      <p className="text-center font-bold text-xl sm:text-2xl lg:text-2xl text-black dark:text-white">
-        {numberWithCommas(value)}
-      </p>
+    <div className="text-center h-min min-h-max m-4 flex flex-col items-center justify-center dark:bg-gray-700 rounded-lg shadow-none bg-white border-2 dark:border-none dark:shadow-md p-4 transition-all duration-300 transform hover:scale-105">
+      {
+        type != "sessions" ? <>
+        <p className="text-center font-semibold text-black dark:font-semibold dark:text-white text-md lg:text-lg xl:text-xl mb-2">
+          {label}:
+        </p>
+        <p className="text-center font-bold text-base sm:text-lg lg:text-2xl text-black dark:text-white">
+          {numberWithCommas(value)}
+        </p></>:  <div className="flex">
+      <p className="text-center mx-auto font-semibold text-black dark:font-semibold dark:text-white text-md lg:text-lg xl:text-xl mb-2">
+        {label}:<span className="ml-1 text-gray-400">{numberWithCommas(value)}</span>
+      </p></div>
+      }
+      {
+        type == "sessions" && value != "..." ? 
+        <>
+          <button onClick={() => setOpen(!open)} className="text-center hover:text-white duration-300 ease-in-out transition flex font-bold text-sm sm:text-base lg:text-lg text-black dark:text-gray-500">
+            {!open ?  <>
+              View Sessions
+              <FontAwesomeIcon className="w-4 h-4 my-auto ml-2" icon={faArrowDown}></FontAwesomeIcon>
+            </> : <>
+              Close List
+              <FontAwesomeIcon className="w-4 h-4 my-auto ml-2" icon={faArrowUp}></FontAwesomeIcon>
+            </> }
+          </button>
+          {
+            open && sessionList.length > 0 ? 
+            <div className="flex px-2 overflow-y-auto max-h-52 w-full flex-col space-y-2">
+              {
+                sessionList.map((session, i) => (
+                  <div className="flex text-start bg-gray-600 rounded">
+                    <div className="flex-col w-11/12 px-2 py-1">
+                      {
+                        userList[i] ?
+                        <p className="inline">
+                        <Link href={`/profile/${userList[i].link}`} className={`text-base no-underline pb-0 hover:text-blue-500 transition duration-300 ease-in-out text-white group font-bold cursor-pointer`}>
+                          {userList[i].username}
+                        </Link>
+                        </p>:
+                        <span className={`text-base cursor-default text-gray-400 font-semibold`}>
+                          A Guest
+                        </span>
+                      }
+                      <p className="text-xs text-gray-400 font-semibold">
+                        {session.view} view
+                      </p>
+                      <p className="text-xs text-gray-400 font-semibold">
+                        {session.message}
+                      </p>
+                    </div>
+                  </div>
+                ))
+              }
+            </div> : <></>
+          }
+        </> : <></>
+      }
     </div>
   );
 }
--- pages/api/info.js
+++ pages/api/info.js
@@ -4,10 +4,10 @@
 // The handler function is an async function that handles the API request. It first checks if the user type is "administrator", if not it returns an error response. Then it calls the getInfo function to fetch the data and sends it back as a JSON response.
 //
 // Overall, this file sets up an API route for retrieving information with iron session authentication.
-import { getInfo } from "@/utils/api";
+import { getAllSessions, getInfo, getOpportunityMongo, getProfileIdFromUserId, getUser, getUserFromMongo, getUserFromProfileId } from "@/utils/api";
 import { ironOptions } from "@/utils/ironConfig";
 import { withIronSessionApiRoute } from "iron-session/next";
-
+import { getOpportunity } from "@/utils/api";
 export default withIronSessionApiRoute(handler, ironOptions)
 
 async function handler(req, res) {
@@ -16,5 +16,62 @@ async function handler(req, res) {
     }
 
     let data = await getInfo();
+    data.info.sessionCount = data.info.sessions
+    let sessionList = await getAllSessions();
+    sessionList = sessionList.sessions
+    let userList = []
+    for(let i = 0; i<sessionList.length; i++) {
+        let session = sessionList[i]
+        let user_id = session.user_id
+        let user = await getUser(user_id)
+        switch(session.view) {
+            case "home":
+                session.message = "is on the homepage"
+                session.link = "/"
+                break;
+            case "profile":
+                let profileViewId = session.viewed_id
+                let user = (await getUser(profileViewId)).user
+                let link = await getProfileIdFromUserId(user.user_id)
+                console.log(`Link: ${link}`)
+                // console.log("User: ", user)
+                session.message = `is viewing ${user.username}'s profile`
+                session.link = `/profile/${link}`
+                break;
+            case "article":
+                break;
+            case "opportunity":
+                let opportunityViewId = session.viewed_id
+                console.log(`Opp ID: ${opportunityViewId}`)
+                let opp = await getOpportunityMongo(opportunityViewId)
+                if(!opp) opp = (await getOpportunity(opportunityViewId)).opportunity
+                session.link = `/opportunity/${opportunityViewId}`
+                session.message = `is looking at an opportunity: ${opp.title}`
+                console.log("opp: ", opp)
+                break;
+            case "admin":
+                session.message = "is on the admin page"
+                session.link = `/admin/${req.session.user.username}`
+                break;
+            case "auth":
+                session.message = "is on the login, signup or forgot password page"
+                session.link = "/auth/signup"
+                break;
+            default:
+                session.message = "is browsing inBDPA"
+                section.link = "/"
+                break;
+        }
+        if(user.user) {
+            // console.log("has user")
+            let link = await getProfileIdFromUserId(user_id)
+            user.user.link = link
+            user.user.salt = null
+        }
+        userList.push(user.user)
+    }
+    data.info.userList = userList
+    data.info.sessions = sessionList
+    // console.log(data.info)
     res.json(data);
 }
